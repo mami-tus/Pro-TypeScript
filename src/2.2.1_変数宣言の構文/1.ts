f2c9cbbda9ba65dad7bab8fc2f46bd8e0f491db2
Mami Nakano <85916526+mami-tus@users.noreply.github.com>
2024-01-04 17:57:58 +0900

    feat: ✨  モジュール認識のためexport追加

--- src/2.2.1_変数宣言の構文/1.ts
+++ src/2.2.1_変数宣言の構文/1.ts
@@ -2,7 +2,7 @@
 複数の変数の宣言を,でつなげることも可能だが、メンテナンス性が落ちるためあまり人気がなく
 変数ひとつにに対してconstを書くやり方が主流
 */
-const greeting = 'Hello,',
+export const greeting = 'Hello,',
   target = 'world!',
   text = greeting + target;
 console.log(text);
