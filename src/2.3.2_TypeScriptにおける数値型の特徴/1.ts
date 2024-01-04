9385732de45b12e3a5758370e7ef94ec4859ab36
Mami Nakano <85916526+mami-tus@users.noreply.github.com>
2024-01-04 18:31:30 +0900

    feat: ✨ p35 number型は整数と小数の区別はない

---
+++ src/2.3.2_TypeScriptにおける数値型の特徴/1.ts
@@ -0,0 +1,8 @@
+export const width1 = 5;
+export const width2 = 8;
+export const height: number = 3;
+const area = ((width1 + width2) * height) / 2;
+// 19.5 と表示される
+console.log(area);
+// 7 と表示される
+console.log(3.5 * 2);
