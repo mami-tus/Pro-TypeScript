57e912af9dfbbec6ada7096b9ac36d51d33e9630
Mami Nakano <85916526+mami-tus@users.noreply.github.com>
2024-01-09 09:11:16 +0900

    feat: ✨ p46 真偽値への型変換

---
+++ src/2.3.10_プリミティブ型同士の変換（2）明示的な変換を行う/6.ts
@@ -0,0 +1,13 @@
+console.log(Boolean(123)); // true
+console.log(Boolean(-123)); // true
+console.log(Boolean(0)); // false
+console.log(Boolean(NaN)); // false
+console.log(Boolean(1n)); // true
+console.log(Boolean(0n)); // false
+console.log(Boolean('foo')); // true
+console.log(Boolean('')); // false
+console.log(Boolean(true)); // true
+console.log(Boolean(false)); // false
+console.log(Boolean(null)); // false
+console.log(Boolean(undefined)); // false
+console.log(Boolean({})); // true
