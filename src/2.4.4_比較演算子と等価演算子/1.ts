7ff093fc61f9119b0ba73713709988025ec52f7c
Mami Nakano <85916526+mami-tus@users.noreply.github.com>
2024-01-12 09:24:07 +0900

    feat: ✨ p51 比較演算子

---
+++ src/2.4.4_比較演算子と等価演算子/1.ts
@@ -0,0 +1,15 @@
+export const left1 = -5,
+  right1 = 0;
+console.log(left1 < right1); // true
+
+export const left2 = 100n,
+  right2 = 50n;
+console.log(left2 >= right2); // true
+
+export const left3 = -10,
+  right3 = 0;
+console.log(left3 > right3); // false
+
+export const left4 = 12n,
+  right4 = 8n;
+console.log(left4 <= right4); // false
