6b1537b12ad7a6f87665735af8c8d529c01c7f97
Mami Nakano <85916526+mami-tus@users.noreply.github.com>
2024-01-13 14:15:26 +0900

    feat: ✨ p54 比較演算子

---
+++ src/2.4.5_論理演算子(1)真偽値の演算/1.ts
@@ -0,0 +1,10 @@
+export const t = true,
+  f = false;
+
+console.log(t && t); // true
+console.log(t && f); // false
+console.log(f && f); // false
+
+console.log(t || t); // true
+console.log(t || f); // true
+console.log(f || f); // false
