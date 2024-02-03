b49f5c1a83f88979165f5bbec317211ca9b75c49
Mami Nakano <85916526+mami-tus@users.noreply.github.com>
2024-02-03 11:57:20 +0900

    feat: ✨ p87 宣言されているプロパティを持っていないとコンパイルエラー

---
+++ src/3.2.2_オブジェクト型の型チェックと安全性/2.ts
@@ -0,0 +1,7 @@
+// // error TS2741: Property 'bar' is missing in type '{ foo: number; }' but required in type '{ foo: number; bar: string; }'.
+// export const obj: {
+//   foo: number;
+//   bar: string;
+// } = {
+//   foo: 123,
+// };
