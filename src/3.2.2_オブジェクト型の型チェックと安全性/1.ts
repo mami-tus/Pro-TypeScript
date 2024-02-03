260ccc616d3a62271dabc2a79921061ed96cfad8
Mami Nakano <85916526+mami-tus@users.noreply.github.com>
2024-02-03 11:53:37 +0900

    feat: ✨ p86 宣言されている型と違う場合コンパイルエラー

---
+++ src/3.2.2_オブジェクト型の型チェックと安全性/1.ts
@@ -0,0 +1,8 @@
+// export const obj: {
+//   foo: number;
+//   bar: string;
+// } = {
+//   foo: 123,
+//   // error TS2322: Type 'boolean' is not assignable to type 'string'.
+//   bar: true,
+// };
