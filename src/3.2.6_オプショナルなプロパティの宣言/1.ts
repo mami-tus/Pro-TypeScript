9f4b5244cfeacc3240cb33140a54613d2f3fea9c
Mami Nakano <85916526+mami-tus@users.noreply.github.com>
2024-02-12 14:29:11 +0900

    feat: ✨ p92 オプショナルなプロパティ宣言

---
+++ src/3.2.6_オプショナルなプロパティの宣言/1.ts
@@ -0,0 +1,15 @@
+type MyObj = {
+  foo: boolean;
+  bar: boolean;
+  baz?: number;
+};
+
+export const obj: MyObj = {
+  foo: true,
+  bar: false,
+};
+export const obj2: MyObj = {
+  foo: true,
+  bar: false,
+  baz: 1234,
+};
