dad91c7502b074e801fa0a2cfda661e58a3c3448
Mami Nakano <85916526+mami-tus@users.noreply.github.com>
2024-02-17 22:06:33 +0900

    feat: ✨ p102 部分型だとエラーにならない

--- src/3.4.3_部分型関係による型引数の制約/1.ts
+++ src/3.4.3_部分型関係による型引数の制約/1.ts
@@ -8,3 +8,14 @@ export type Family<Parent extends HasName, Child extends HasName> = {
 };
 // error TS2344: Type 'number' does not satisfy the constraint 'HasName'.
 // export type T = Family<number, string>;
+
+// HumanやAnimalはHasNameの部分型なのでエラーにならない
+export type Animal = {
+  name: string;
+};
+export type Human = {
+  name: string;
+  age: number;
+};
+
+type T = Family<Animal, Human>;
