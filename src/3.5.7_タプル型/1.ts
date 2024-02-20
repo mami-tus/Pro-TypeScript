bf0eb1d91c18f855e4ba9cc67940caa7c51128fa
Mami Nakano <85916526+mami-tus@users.noreply.github.com>
2024-02-20 23:26:30 +0900

    feat: ✨ p113 readonly, オプショナル

--- src/3.5.7_タプル型/1.ts
+++ src/3.5.7_タプル型/1.ts
@@ -5,3 +5,10 @@ export const str = tuple[0]; // strはstring型
 export const num = tuple[1]; // numはnumber型
 // error TS2493: Tuple type '[string, number]' of length '2' has no element at index '2'.
 // export const nothing = tuple[2];
+
+export const tuple2: readonly [string, number] = ['hoge', 10];
+//  error TS2540: Cannot assign to '0' because it is a read-only property.
+// tuple2[0] = 'foo';
+
+export const tuple3: [string, number, string?] = ['hoge', 10, 'foo'];
+export const foo = tuple3[2]; // fooはstring | undefined型
