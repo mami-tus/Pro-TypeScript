fc08b1d3f331ae1c8fe9ac4b78fc7dd0507ae919
Mami Nakano <85916526+mami-tus@users.noreply.github.com>
2024-02-27 13:55:40 +0900

    feat: ✨ p122 Dateオブジェクトのメソッド

--- src/3.7.1_Dateオブジェクト/1.ts
+++ src/3.7.1_Dateオブジェクト/1.ts
@@ -1,2 +1,10 @@
 const d = new Date();
 console.log(d); // 現在の日時と時間が表示される
+
+// 日時データの中身を取得する
+console.log(d.getFullYear()); // 2024
+console.log(d.getMonth()); // 1 (0から始まる)
+
+// 日時データの書き換え
+console.log(d.setFullYear(2030));
+console.log(d.getFullYear()); // 2030
