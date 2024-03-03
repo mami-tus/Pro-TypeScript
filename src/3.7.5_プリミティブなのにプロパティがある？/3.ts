97dd8e4ea0b982872e6415fbfab2554d536fb2e2
Mami Nakano <85916526+mami-tus@users.noreply.github.com>
2024-03-03 21:27:57 +0900

    feat: ✨ p129 {}型はnull,undefined以外受け付ける

---
+++ src/3.7.5_プリミティブなのにプロパティがある？/3.ts
@@ -0,0 +1,7 @@
+// これらはOK
+let val: {} = 123;
+val = 'foobar';
+val = { num: 1234 };
+// これらはコンパイルエラー
+// val = null; // error TS2322: Type 'null' is not assignable to type '{}'.
+// val = undefined; // error TS2322: Type 'undefined' is not assignable to type '{}'.
