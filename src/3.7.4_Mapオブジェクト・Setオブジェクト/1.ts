1577c97f763378fed1c22a2a65448c634a645766
Mami Nakano <85916526+mami-tus@users.noreply.github.com>
2024-03-03 20:46:27 +0900

    feat: ✨ p127 Mapオブジェクトのさまざまなメソッド

--- src/3.7.4_Mapオブジェクト・Setオブジェクト/1.ts
+++ src/3.7.4_Mapオブジェクト・Setオブジェクト/1.ts
@@ -1,5 +1,13 @@
 export const map: Map<string, number> = new Map();
 map.set('foo', 1234);
 
+// error TS2345: Argument of type 'string' is not assignable to parameter of type 'number'.
+// map.set('foo', '1234');
+
 console.log(map.get('foo')); // 1234
 console.log(map.get('bar')); // undefined
+console.log(map.has('foo')); // true
+console.log(map.has('bar')); // false
+console.log(map.delete('foo')); // true
+console.log(map.delete('bar')); // false
+console.log(map.has('foo')); // false
