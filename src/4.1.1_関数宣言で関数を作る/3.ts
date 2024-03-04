4593b0d9b9fd79e43ddf7503e0dd7bed28a93ca5
Mami Nakano <85916526+mami-tus@users.noreply.github.com>
2024-03-05 08:00:51 +0900

    feat: ✨ p137 巻上げ

---
+++ src/4.1.1_関数宣言で関数を作る/3.ts
@@ -0,0 +1,9 @@
+console.log(range(5, 10)); // [5, 6, 7, 8, 9, 10]
+
+export function range(min: number, max: number): number[] {
+  const result = [];
+  for (let i = min; i <= max; i++) {
+    result.push(i);
+  }
+  return result;
+}
