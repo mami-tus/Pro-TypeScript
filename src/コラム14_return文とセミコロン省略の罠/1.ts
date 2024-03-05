fd0241ce4f5a7f5618494de54d4a24ef4db1dd3c
Mami Nakano <85916526+mami-tus@users.noreply.github.com>
2024-03-05 09:25:51 +0900

    feat: ✨ p139 returnの後改行しない

---
+++ src/コラム14_return文とセミコロン省略の罠/1.ts
@@ -0,0 +1,8 @@
+// export function toSeconds(
+//   hours: number,
+//   minutes: number,
+//   seconds: number
+// ): number {
+//   // return  // このセミコロンが省略されたと思われる
+//   hours * 3600 + minutes * 60 + seconds;
+// }
