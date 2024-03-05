a1866eadfb4c8f6df75545488617e7652ad35ad0
Mami Nakano <85916526+mami-tus@users.noreply.github.com>
2024-03-06 06:35:18 +0900

    feat: ✨ p142 function関数式よりアロー関数式が好まれる

--- src/4.1.4_アロー関数式を作る/1.ts
+++ src/4.1.4_アロー関数式を作る/1.ts
@@ -6,6 +6,10 @@ export type Human = {
 export const calcBMI = ({ height, weight }: Human): number => {
   return weight / height ** 2;
 };
+// function 関数式の場合
+// export const calcBMI = function ({ height, weight }: Human): number {
+//   return weight / height ** 2;
+// };
 
 export const uhyo: Human = {
   height: 1.84,
