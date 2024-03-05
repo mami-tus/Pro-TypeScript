474e76e28986ec63750b1b2adacac604849a190f
Mami Nakano <85916526+mami-tus@users.noreply.github.com>
2024-03-06 07:20:38 +0900

    feat: ✨ p144 メソッド記法

---
+++ src/4.1.6_メソッド記法で関数を作る/1.ts
@@ -0,0 +1,11 @@
+export const obj = {
+  // メソッド記法
+  double(num: number): number {
+    return num * 2;
+  },
+  // 通常の記法 + アロー関数
+  double2: (num: number): number => num * 2,
+};
+
+console.log(obj.double(100)); // 200
+console.log(obj.double2(-50)); // -100
