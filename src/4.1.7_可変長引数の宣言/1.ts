86f223cf5ede076d6fc5b0ae127f15ff8719e5d4
Mami Nakano <85916526+mami-tus@users.noreply.github.com>
2024-03-06 07:38:52 +0900

    feat: ✨ p144 可変長引数

---
+++ src/4.1.7_可変長引数の宣言/1.ts
@@ -0,0 +1,11 @@
+export const sum = (...args: number[]): number => {
+  let result = 0;
+  for (const num of args) {
+    result += num;
+  }
+  return result;
+};
+
+console.log(sum(1, 10, 100)); // 111
+console.log(sum(123, 456)); // 579
+console.log(sum()); // 0
