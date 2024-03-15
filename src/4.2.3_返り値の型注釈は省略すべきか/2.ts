d69963bcbe095dc34f4aaf79b6cd2e60a58835f9
Mami Nakano <85916526+mami-tus@users.noreply.github.com>
2024-03-16 01:50:41 +0900

    feat: ✨ p155 returnを忘れると型エラー

---
+++ src/4.2.3_返り値の型注釈は省略すべきか/2.ts
@@ -0,0 +1,11 @@
+// error TS2355: A function whose declared type is neither 'undefined', 'void', nor 'any' must return a value.
+// export function range(min: number, max: number): number[] {
+//   const result: number[] = [];
+//   for (let i = min; i <= max; i++) {
+//     result.push(i);
+//   }
+// }
+
+// const arr = range(5, 10);
+// ループ内の処理が一行だけの場合は、ブロックを省略できる
+// for (const value of arr) console.log(value);
