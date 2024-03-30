2031d664e1b3e7705fe6a054054c6a8633bee806
Mami Nakano <85916526+mami-tus@users.noreply.github.com>
2024-03-30 16:54:55 +0900

    feat: ✨ p170 型引数と違う型だとコンパイルエラー

--- src/4.4.1_関数の型引数とは/1.ts
+++ src/4.4.1_関数の型引数とは/1.ts
@@ -10,6 +10,10 @@ function repeat<T>(element: T, length: number): T[] {
 }
 
 // [ 'a', 'a', 'a', 'a', 'a' ] が表示される
+// 型は function repeat<string>(element: string, length: number): string[] となる
 console.log(repeat<string>('a', 5));
 // [ 123, 123, 123 ] が表示される
 console.log(repeat<number>(123, 3));
+
+//  error TS2345: Argument of type 'number' is not assignable to parameter of type 'string'.
+// repeat<string>(0, 10);
