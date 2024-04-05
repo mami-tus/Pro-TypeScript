9eb3f0347d4036b0c8e2f54a6a68abf01f263f02
Mami Nakano <85916526+mami-tus@users.noreply.github.com>
2024-04-05 21:26:07 +0900

    feat: ✨ p191 プロパティ初期化できてないとエラー

---
+++ src/5.1.4_コンストラクタ/2.ts
@@ -0,0 +1,9 @@
+export class User {
+  name: string;
+  // error TS2564: Property 'age' has no initializer and is not definitely assigned in the constructor.
+  // age: number;
+
+  constructor(name: string, age: number) {
+    this.name = name;
+  }
+}
