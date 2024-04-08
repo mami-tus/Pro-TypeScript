09eab86e6d831f86dc66fb3844530f4396697395
Mami Nakano <85916526+mami-tus@users.noreply.github.com>
2024-04-08 22:34:54 +0900

    feat: ✨ p206 クラスオブジェクトの型

---
+++ src/5.2.2_newシグネチャによるインスタンス化可能性の表現/1.ts
@@ -0,0 +1,13 @@
+export class User {
+  name: string = '';
+  age: number = 0;
+}
+
+export type MyUserConstructor = new () => User;
+
+// UserはMyUserConstructor型を持つ
+export const MyUser: MyUserConstructor = User;
+// MyUserはnewで使用可能
+export const u = new MyUser();
+// uはUser型を持つ
+console.log(u.name, u.age);
