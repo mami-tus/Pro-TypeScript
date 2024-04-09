e312e82a9d9a242e3bd926528f0bb5c1c5a6241d
Mami Nakano <85916526+mami-tus@users.noreply.github.com>
2024-04-09 18:18:32 +0900

    feat: ✨ p212 コンストラクタをオーバーライド
    super呼び出しを付与する

---
+++ src/5.3.2_継承（2）親の機能を上書きする/3.ts
@@ -0,0 +1,26 @@
+export class User {
+  name: string;
+  #age: number;
+
+  constructor(name: string, age: number) {
+    this.name = name;
+    this.#age = age;
+  }
+
+  public isAdult(): boolean {
+    return this.#age >= 20;
+  }
+}
+
+export class PremiumUser extends User {
+  rank: number = 1;
+
+  constructor(name: string, age: number, rank: number) {
+    super(name, age);
+    this.rank = rank;
+  }
+}
+
+export const uhyo = new PremiumUser('uhyo', 26, 3);
+console.log(uhyo.name); // uhyo と表示される
+console.log(uhyo.rank); // 3 と表示される
