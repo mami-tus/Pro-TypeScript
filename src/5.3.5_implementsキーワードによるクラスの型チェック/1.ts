2a15686279bcac28b0578441bc3340c58d77e96d
Mami Nakano <85916526+mami-tus@users.noreply.github.com>
2024-04-10 22:06:17 +0900

    feat: ✨ p220 implementsで部分型である宣言

---
+++ src/5.3.5_implementsキーワードによるクラスの型チェック/1.ts
@@ -0,0 +1,17 @@
+export type HasName = {
+  name: string;
+};
+
+export class User implements HasName {
+  name: string;
+  #age: number;
+
+  constructor(name: string, age: number) {
+    this.name = name;
+    this.#age = age;
+  }
+
+  public isAdult(): boolean {
+    return this.#age >= 20;
+  }
+}
