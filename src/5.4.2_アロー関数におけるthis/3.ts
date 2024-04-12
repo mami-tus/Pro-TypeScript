8745b4bc21704a52e72cd4d3d04ef9dc946fba03
Mami Nakano <85916526+mami-tus@users.noreply.github.com>
2024-04-13 00:12:21 +0900

    feat: ✨ p225 thisの型注釈はランタイムエラーになる

--- src/5.4.2_アロー関数におけるthis/3.ts
+++ src/5.4.2_アロー関数におけるthis/3.ts
@@ -11,9 +11,19 @@ export class User {
     return this.#age >= 20;
   }
 
+  // 以下はコンパイルエラーにはならないが、ランタイムエラーになる
+  // users.filter は this が uhyo になることを知らないため
   public filterOlder(users: readonly User[]): User[] {
     return users.filter(function (this: User, u) {
       u.#age > this.#age;
     });
   }
 }
+
+export const uhyo = new User('uhyo', 25);
+export const john = new User('John Smith', 15);
+export const bob = new User('Bob', 40);
+
+export const older = uhyo.filterOlder([john, bob]);
+// [ User { name: 'Bob' } ] と表示される
+console.log(older);
