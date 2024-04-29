80d77a5d0b1a2d4d34435876e26ca111bdb2441d
Mami Nakano <85916526+mami-tus@users.noreply.github.com>
2024-04-29 17:56:34 +0900

    feat: ✨ p255 ユニオン型の関数型の関数を呼び出すとコンパイルエラー

---
+++ src/6.1.4_ユニオン型とインターセクション型の表裏一体な関係/1.ts
@@ -0,0 +1,18 @@
+export type Human = { name: string };
+export type Animal = { species: string };
+function getName(human: Human) {
+  return human.name;
+}
+function getSpecies(animal: Animal) {
+  return animal.species;
+}
+const mysteryFunc = Math.random() < 0.5 ? getName : getSpecies;
+
+export const uhyo: Human = { name: 'uhyo' };
+export const cat: Animal = { species: 'Felis silvestris catus' };
+// Property 'species' is missing in type 'Human' but required in type 'Animal'.
+// error TS2345: Argument of type 'Human' is not assignable to parameter of type 'Human & Animal'.
+// mysteryFunc(uhyo);
+// error TS2345: Argument of type 'Animal' is not assignable to parameter of type 'Human & Animal'.
+// Property 'name' is missing in type 'Animal' but required in type 'Human'.
+// mysteryFunc(cat);
