3930e481d32ccece3f54ce4549174568b3a12193
Mami Nakano <85916526+mami-tus@users.noreply.github.com>
2024-04-29 18:05:51 +0900

    feat: ✨ p256 ユニオン型の関数型はインターセクション型の引数なら呼び出せる

--- src/6.1.4_ユニオン型とインターセクション型の表裏一体な関係/1.ts
+++ src/6.1.4_ユニオン型とインターセクション型の表裏一体な関係/1.ts
@@ -10,8 +10,8 @@ const mysteryFunc = Math.random() < 0.5 ? getName : getSpecies;
 
 export const uhyo: Human = { name: 'uhyo' };
 export const cat: Animal = { species: 'Felis silvestris catus' };
-// Property 'species' is missing in type 'Human' but required in type 'Animal'.
 // error TS2345: Argument of type 'Human' is not assignable to parameter of type 'Human & Animal'.
+// Property 'species' is missing in type 'Human' but required in type 'Animal'.
 // mysteryFunc(uhyo);
 // error TS2345: Argument of type 'Animal' is not assignable to parameter of type 'Human & Animal'.
 // Property 'name' is missing in type 'Animal' but required in type 'Human'.
