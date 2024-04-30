ded2ad3de70dfe62a1bacdab3741d8a856eeaa5c
Mami Nakano <85916526+mami-tus@users.noreply.github.com>
2024-04-30 23:11:47 +0900

    feat: ✨ p257 オプショナルプロパティ

---
+++ src/6.1.5_オプショナルプロパティ再訪/1.ts
@@ -0,0 +1,13 @@
+export type Human = {
+  name: string;
+  age?: number;
+};
+
+export const uhyo: Human = {
+  name: 'uhyo',
+  age: 25,
+};
+
+export const tom: Human = {
+  name: 'John Smith',
+};
