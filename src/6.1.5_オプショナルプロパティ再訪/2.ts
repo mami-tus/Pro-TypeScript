a5ef4c9d1a109a212969567b2534ccd5b0dbbdca
mamitus <85916526+mami-tus@users.noreply.github.com>
2025-01-13 17:26:11 +0900

    exactOptionalPropertyTypesオプション

--- src/6.1.5_オプショナルプロパティ再訪/2.ts
+++ src/6.1.5_オプショナルプロパティ再訪/2.ts
@@ -5,5 +5,7 @@ export type Human = {
 
 export const john: Human = {
   name: 'John Smith',
+  // exactOptionalPropertyTypes が有効の場合以下のコンパイルエラーが発生
+  //  プロパティ 'age' の型に互換性がありません。型 'undefined' を型 'number' に割り当てることはできません。ts(2375)
   age: undefined,
 };
