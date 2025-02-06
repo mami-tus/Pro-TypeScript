472e36a153eb0d8435c56433daa1158fd048a5cd
mamitus <85916526+mami-tus@users.noreply.github.com>
2025-02-06 23:38:01 +0900

    6.3.2_typeof演算子を用いる絞り込み

---
+++ src/6.3.2_typeof演算子を用いる絞り込み/1.ts
@@ -0,0 +1,5 @@
+console.log(typeof 'uhyo'); // "string" と表示される
+console.log(typeof 42); // "number" と表示される
+console.log(typeof {}); // "object" と表示される
+console.log(typeof undefined); // "undefined" と表示される
+console.log(typeof null); // "object" と表示される
