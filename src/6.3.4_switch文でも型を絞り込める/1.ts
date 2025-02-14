78bc2c2aeea45ee41a8a1473f84815848e3182be
mamitus <85916526+mami-tus@users.noreply.github.com>
2025-02-14 16:38:08 +0900

    6.3.4_switch文でも型を絞り込める_2

--- src/6.3.4_switch文でも型を絞り込める/1.ts
+++ src/6.3.4_switch文でも型を絞り込める/1.ts
@@ -6,13 +6,27 @@ export type Human = {
   tag: 'human';
   name: string;
 };
-export type User = Animal | Human;
+export type Robot = {
+  tag: 'robot';
+  name: string;
+};
+export type User = Animal | Human | Robot;
 
-export function getUserName(user: User): string {
+export function getUserName1(user: User): string {
+  if (user.tag === 'human') {
+    return user.name;
+  } else {
+    return '名無し';
+  }
+}
+
+export function getUserName2(user: User): string {
   switch (user.tag) {
     case 'human':
       return user.name;
     case 'animal':
       return '名無し';
+    case 'robot':
+      return `CPU ${user.name}`;
   }
 }
