474a260d14b27da13b027ce43fc5ad693dc68e0a
mamitus <85916526+mami-tus@users.noreply.github.com>
2025-02-20 00:30:00 +0900

    keyof typeof

--- src/6.4.2_keyof型とは/2.ts
+++ src/6.4.2_keyof型とは/2.ts
@@ -1,5 +1,6 @@
 export const mmConversionTable = {
   mm: 1,
+  cm: 10, // この行追加
   m: 1e3,
   km: 1e6,
 };
@@ -18,3 +19,5 @@ export function convertUnits(
 
 // { mm: 5600000, m: 5600, km: 5.6 }
 console.log(convertUnits(5600, 'm'));
+// { mm: 3000000, m: 3000, km: 3 }
+console.log(convertUnits(300000, 'cm')); // この行追加
