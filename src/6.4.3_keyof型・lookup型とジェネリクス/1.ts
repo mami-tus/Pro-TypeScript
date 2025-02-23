38c33b89e251dec869eb5b8824cea6373af1b98c
mamitus <85916526+mami-tus@users.noreply.github.com>
2025-02-23 23:39:16 +0900

    src/6.4.3_keyof型・lookup型とジェネリクス/1.ts_2

--- src/6.4.3_keyof型・lookup型とジェネリクス/1.ts
+++ src/6.4.3_keyof型・lookup型とジェネリクス/1.ts
@@ -1,3 +1,4 @@
+// K extends keyof T> がないとT[K]というlookup型が正しいかわからずコンパイルエラーになる
 export function get<T, K extends keyof T>(obj: T, key: K): T[K] {
   return obj[key];
 }
@@ -16,3 +17,5 @@ export const uhyo: Human = {
 export const uhyoName = get(uhyo, 'name');
 // uhyoAgeはnumber型
 export const uhyoAge = get(uhyo, 'age');
+// 型 '"gender"' の引数を型 'keyof Human' のパラメーターに割り当てることはできません。ts(2345)
+// export const uhyoGender = get(uhyo, 'gender');
