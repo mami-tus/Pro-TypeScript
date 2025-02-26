cfb4b72bba2e77daf980a6c866f23d5bbb7510ec
mamitus <85916526+mami-tus@users.noreply.github.com>
2025-02-26 23:58:50 +0900

    src/6.5.1_ 型アサーションを用いて式の型をごまかす/2.ts

---
+++ src/6.5.1_ 型アサーションを用いて式の型をごまかす/2.ts
@@ -0,0 +1,20 @@
+export type Animal = {
+  tag: 'animal';
+  species: string;
+};
+export type Human = {
+  tag: 'human';
+  name: string;
+};
+export type User = Animal | Human;
+
+export function getNmaesIfAllHuman(
+  users: readonly User[]
+): string[] | undefined {
+  if (users.every((user) => user.tag === 'human')) {
+    // error TS2339: Property 'name' does not exist on type 'User'.
+    // Property 'name' does not exist on type 'Animal'.
+    return users.map((user) => user.name);
+  }
+  return undefined;
+}
