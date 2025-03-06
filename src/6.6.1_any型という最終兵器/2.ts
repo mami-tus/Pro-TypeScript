7fe40aeed81bf3cb2c0863f20e31e24960f41c4c
mamitus <85916526+mami-tus@users.noreply.github.com>
2025-03-06 21:11:53 +0900

    src/6.6.1_any型という最終兵器/2.ts

---
+++ src/6.6.1_any型という最終兵器/2.ts
@@ -0,0 +1,10 @@
+export function useNumber(num: number) {
+  console.log(num);
+}
+
+export function doWhatever(obj: any) {
+  // string型の変数に入れられる
+  const str: string = obj;
+  // number型を要求する関数に渡せる
+  useNumber(obj);
+}
