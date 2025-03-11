b98a3ac6a79b37e55fb71b00d27b243ad5937806
mamitus <85916526+mami-tus@users.noreply.github.com>
2025-03-11 21:02:52 +0900

    src/6.7.1_object型・never型/2.ts

--- src/6.7.1_object型・never型/2.ts
+++ src/6.7.1_object型・never型/2.ts
@@ -1,5 +1,21 @@
-export function doNothing(val: unknown) {
-  // 'val''は 'unknown' 型です。ts(18046)
-  // const name = val.name;
-  console.log(val);
+// toStringを持つ値の型
+export type HasToString = {
+  toString: () => string;
+};
+
+export function useToString2(value: HasToString & object) {
+  console.log(`value is ${value.toString()}`);
 }
+
+// "value is foo!"と表示される
+useToString2({
+  toString() {
+    return 'foo!';
+  },
+});
+
+/**
+ * Argument of type 'number' is not assignable to parameter of type 'HasToString & object'.
+  Type 'number' is not assignable to type 'object'.ts(2345)
+ */
+// useToString2(3.14);
