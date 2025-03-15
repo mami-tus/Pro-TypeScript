d970c6d3c45887d36fdc8d8a752bcfaec418f089
mamitus <85916526+mami-tus@users.noreply.github.com>
2025-03-15 23:06:13 +0900

    src/6.7.2_型述語（ユーザー定義型ガード）/2.ts

--- src/6.7.2_型述語（ユーザー定義型ガード）/2.ts
+++ src/6.7.2_型述語（ユーザー定義型ガード）/2.ts
@@ -1,11 +1,11 @@
-export function isStringOrNumber(value: unknown): value is string | number {
+export function isStringOrNumber(value: unknown): boolean {
   return typeof value === 'string' || typeof value === 'number';
 }
 
 const something: unknown = 123;
 
 if (isStringOrNumber(something)) {
-  // somethingはstring | number型
-  console.log(something.toString());
+  // 'something' is of type 'unknown'.ts(18046)
+  // console.log(something.toString());
 }
 //https://zenn.dev/ubie_dev/articles/ts-infer-type-predicates
