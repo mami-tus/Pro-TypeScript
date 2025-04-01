84190e3f65074d3e9f66226ac2e39b951ca7559a
mamitus <85916526+mami-tus@users.noreply.github.com>
2025-04-01 09:26:35 +0900

    Add counter and age modules with increment functionality

---
+++ src/7.1.3_defaultエクスポートとdefaultインポート/index.ts
@@ -0,0 +1,6 @@
+import increment, { value } from './counter.js';
+
+console.log(`カウンタの値は${increment()}です`);
+console.log(`カウンタの値は${increment()}です`);
+console.log(`カウンタの値は${increment()}です`);
+console.log(`カウンタの値は${value}です`);
