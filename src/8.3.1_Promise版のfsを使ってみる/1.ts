050586e79117206c7207cdb9b8c831ae760f6ad8
mamitus <85916526+mami-tus@users.noreply.github.com>
2025-04-15 07:32:41 +0900

    feat: uhyo.txtを読み込むPromiseのサンプルを追加

---
+++ src/8.3.1_Promise版のfsを使ってみる/1.ts
@@ -0,0 +1,7 @@
+import { readFile } from 'fs/promises';
+
+const p = readFile('uhyo.txt', 'utf8');
+
+p.then((data) => {
+  console.log(data);
+});
---
+++ src/8.3.1_Promise版のfsを使ってみる/2.ts
@@ -0,0 +1,13 @@
+import { readFile } from 'fs/promises';
+
+const p = readFile('uhyo.txt', 'utf8');
+
+p.then((result) => {
+  console.log('1');
+});
+p.then((result) => {
+  console.log('2');
+});
+p.then((result) => {
+  console.log('3');
+});
