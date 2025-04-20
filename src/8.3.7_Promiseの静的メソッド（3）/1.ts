155e4cffc3402aad8784c75f07c139bbdfc77f10
mamitus <85916526+mami-tus@users.noreply.github.com>
2025-04-20 23:45:29 +0900

    feat: uhyo.txtを読み込むPromiseのサンプルを追加

---
+++ src/8.3.7_Promiseの静的メソッド（3）/1.ts
@@ -0,0 +1,16 @@
+import { readFile } from 'fs/promises';
+
+export const sleepReject = (duration: number) => {
+  return new Promise<never>((resolve, reject) => {
+    setTimeout(reject, duration);
+  });
+};
+
+export const p = Promise.allSettled([
+  readFile('uhyo.txt', 'utf8'),
+  sleepReject(5000),
+]);
+
+p.then((result) => {
+  console.log(result);
+});
